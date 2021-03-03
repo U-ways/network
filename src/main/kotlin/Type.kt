c0d904826b68c123221af4f824d607ace042cece
u-ways <ue95.career@gmail.com>
2021-03-03 05:14:54 +0000

    Clarify frame type enum comments

--- src/main/kotlin/Type.kt
+++ src/main/kotlin/Type.kt
@@ -1,4 +1,4 @@
 enum class Type {
-    D, // data frame, but not final segment of message
-    F, // data frame containing final segment of message
+    D,  // Non-final data frame.
+    F,  // Final frame of message.
 }
